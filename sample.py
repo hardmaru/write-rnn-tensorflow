e6dc9a5b8c7d00372aaaf8b7c073fc5abcb67894
Memo Akten <memo@memo.tv>
2017-02-25 18:57:17 +0000

    freeze graph

--- sample.py
+++ sample.py
@@ -25,6 +25,9 @@ parser.add_argument('--scale_factor', type=int, default=10,
                    help='factor to scale down by for svg output.  smaller means bigger output')
 parser.add_argument('--model_dir', type=str, default='save',
                    help='directory to save model to')
+parser.add_argument('--freeze_graph', dest='freeze_graph', action='store_true',
+                   help='if true, freeze (replace variables with consts), prune (for inference) and save graph')
+	
 sample_args = parser.parse_args()
 
 with open(os.path.join(sample_args.model_dir, 'config.pkl'), 'rb') as f:
@@ -48,6 +51,19 @@ def sample_stroke():
   draw_strokes_eos_weighted(strokes, params, factor=sample_args.scale_factor, svg_filename = sample_args.filename+'.eos_pdf.svg')
   draw_strokes_pdf(strokes, params, factor=sample_args.scale_factor, svg_filename = sample_args.filename+'.pdf.svg')
   return [strokes, params]
+
+
+def freeze_and_save_graph(sess, folder, out_nodes, as_text=False):
+    ## save graph definition
+    graph_raw = sess.graph_def
+    graph_frz = tf.graph_util.convert_variables_to_constants(sess, graph_raw, out_nodes)
+    ext = '.txt' if as_text else '.pb'
+    #tf.train.write_graph(graph_raw, folder, 'graph_raw'+ext, as_text=as_text)
+    tf.train.write_graph(graph_frz, folder, 'graph_frz'+ext, as_text=as_text)
+    
+
+if(sample_args.freeze_graph):
+    freeze_and_save_graph(sess, sample_args.model_dir, ['data_out', 'state_out'], False)
 
 [strokes, params] = sample_stroke()
 
